a3c230ca3123119be17012d0e33d5e61a93c71a9
AndrewZhouMQ123 <lolserverhascrashed@gmail.com>
2025-02-21 01:09:07 -0800

    taking a break, development paused, quick touch up on home and about

--- src/app/api/stripe.ts
+++ src/app/api/stripe.ts
@@ -1,4 +1,4 @@
-import { loadStripe } from "@stripe/stripe-js";
+// import { loadStripe } from "@stripe/stripe-js";
 
-const publishable_key = "pk_test_51Qu0VHPUdz2Zv0MfQVNyngABRD7ysXHBV8uQuXX9bF7iEMuoO97CDJ8MmwCRikcfLM2IrI7QKQiImpY6ieJfn36b001cheXo1R"
-const stripePromise = loadStripe(publishable_key);
+// const publishable_key = "pk_test_51Qu0VHPUdz2Zv0MfQVNyngABRD7ysXHBV8uQuXX9bF7iEMuoO97CDJ8MmwCRikcfLM2IrI7QKQiImpY6ieJfn36b001cheXo1R"
+// const stripePromise = loadStripe(publishable_key);
--- src/app/graph/page.tsx
+++ src/app/graph/page.tsx
@@ -1,6 +1,14 @@
 const GraphHome = () => {
   return (
     <div>
+      <h1>PS from website owner:</h1>
+      <p>
+        I am currently busy this week.
+        I will setup the API and write documentation explaining how to use the API.
+        In the mean time, check the README on my github page.
+        https://github.com/AndrewZhouMQ123/Sci_Graph_API
+        Feel free to go through the code if you are interested.
+      </p>
     </div>
   );
 };
